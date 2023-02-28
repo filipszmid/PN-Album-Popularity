ff677cbe00b9e1f840dc4e2564a71264d446c0dc
filip.szmid1 <filip.szmid@partners.medialabgroup.co.uk>
2023-02-28 16:30:57 +0100

    feat: adjustment

--- classifier/stages/1_data_preprocessing.py
+++ classifier/stages/1_data_preprocessing.py
@@ -1,25 +1,9 @@
-# Imports you may need
-# Feel free to import other librariers (especially for the ML parts)
 import pandas as pd
 from pathlib import Path
-
-# datetime operations
-
-# multiprocessing
-
-# ttest and euclidean distance
-
-# linear fit using statsmodels
-
-# sklearn
-
-
 from utils import replace_genre, fix_paths
 
 
 fix_paths()
-
-
 data_path = Path("../data/pitchfork.csv")
 org_df = pd.read_csv(data_path, dtype={"releaseyear": int})
 org_df.head()
@@ -30,7 +14,6 @@ org_df["genre"] = org_df["genre"].apply(replace_genre)
 df = org_df.drop_duplicates(
   subset = ['album', 'reviewauthor'],
   keep = 'first').reset_index(drop = True)
-
 
 df.to_csv(r"../data/bucket/preprocessed-pitchfork.csv", index=True)
 
